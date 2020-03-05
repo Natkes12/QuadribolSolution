9998400bbe2219bed4a317c203a421b7fe939d90
Natanael Junkes <natanael.junkes@gmail.com>
2020-03-05 16:58:39 -0300

    Entidade Competidor

---
+++ Entity/Competidor.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Enums;
+using System;
+
+namespace Entity
+{
+    public class Competidor
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public Casa Casa { get; set; }
+        public string Escolaridade { get; set; }
+        public Funcao Funcao { get; set; }
+        public bool Disponivel { get; set; }
+        public bool EhAtivo { get; set; }
+    }
+}
