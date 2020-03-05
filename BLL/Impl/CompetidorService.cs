0724066da0b9073f012d6dbb8c3a0fe1429dde71
Caroline Pailer <carolinepailer@gmail.com>
2020-03-05 17:12:17 -0300

    Criação do BLL

--- BLL/Impl/CompetidorService.cs
+++ BLL/Impl/CompetidorService.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using BLL.Impl;
+using BLL.Interfaces;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BLL
 {
-    public class CompetidorService
+    public class CompetidorService : ICompetidorService
     {
+        public Task<List<Competidor>> GetCompetidores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(Competidor competidor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
