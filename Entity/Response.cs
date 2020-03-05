bf9d4a99e3efcb019f4623359bd064b6eb15f02b
Caroline Pailer <carolinepailer@gmail.com>
2020-03-05 17:17:09 -0300

    Response criado

---
+++ Entity/Response.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class Response
+    {
+        public bool Sucesso { get; set; }
+        public List<string> Erros { get; set; }
+
+        public Response()
+        {
+            this.Erros = new List<string>();
+        }
+
+        public bool HasErrors()
+        {
+            return this.Erros.Count > 0;
+        }
+
+        public string GetErrorMessage()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string item in this.Erros)
+            {
+                builder.AppendLine(item);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
