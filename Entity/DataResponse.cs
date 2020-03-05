89f6683c8efbcd67bb723d44f93400d7f36ed792
Caroline Pailer <carolinepailer@gmail.com>
2020-03-05 17:19:27 -0300

    DataResponse criado

---
+++ Entity/DataResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class DataResponse<T> : Response
+    {
+        public List<T> Data { get; set; }
+    }
+}
