ed0e5f73cc4b2479742ecec084c2e29dfc4ee011
Natanael Junkes <natanael.junkes@gmail.com>
2020-03-06 16:59:46 -0300

    Todos os Repositories feitos

---
+++ DAO/Impl/NarradorRepository.cs
@@ -0,0 +1,29 @@
+﻿using DAO.Interfaces;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Impl
+{
+    public class NarradorRepository : INarradorRepository
+    {
+        private readonly QuadribolContext _context;
+
+        public NarradorRepository(QuadribolContext context)
+        {
+            this._context = context;
+        }
+
+        public Task<DataResponse<Narrador>> GetNarradores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(Narrador narrador)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
