eece3ba3f429236909dceb7c040a03a17a96643d
Caroline Pailer <carolinepailer@gmail.com>
2020-03-06 14:16:23 -0300

    ITimeService criado

---
+++ BLL/Interfaces/ITimeService.cs
@@ -0,0 +1,14 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ITimeService
+    {
+        Task Insert(Time time);
+        Task<List<Time>> GetTimes();
+    }
+}
