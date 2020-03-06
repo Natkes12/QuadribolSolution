ff1a3093a23b4a58f6e1bc1ae996e48601f0db60
Natanael Junkes <natanael.junkes@gmail.com>
2020-03-06 15:40:51 -0300

    Alteração em Repositories

--- DAO/CompetidorRepository.cs
+++ DAO/CompetidorRepository.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using DAO.Interfaces;
+using Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DAO
 {
-    public class CompetidorRepository
+    public class CompetidorRepository : ICompetidorRepository
     {
+        public async Task<DataResponse<Competidor>> GetCompetidores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response> Insert(Competidor competidor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
