f22b8e9bafabc7c299095469ce505ea99557ba22
Caroline Pailer <carolinepailer@gmail.com>
2020-03-09 14:57:08 -0300

    Alteração no Service

--- BLL/Impl/CompetidorService.cs
+++ BLL/Impl/CompetidorService.cs
@@ -27,17 +27,7 @@ namespace BLL
             if (string.IsNullOrWhiteSpace(competidor.Nome))
             {
                 response.Erros.Add("O nome do competidor deve ser informado.");
-            }
-
-            if (string.IsNullOrWhiteSpace(competidor.Escolaridade))
-            {
-                response.Erros.Add("A escolaridade deve ser informada.");
-            }
-
-            if (competidor.Escolaridade.Length < 4)
-            {
-                response.Erros.Add("O competidor deve ter escolaridade de no mínimo 4.");
-            }
+            }          
 
             if (response.Erros.Count > 0)
             {
