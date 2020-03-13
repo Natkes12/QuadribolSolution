a5d14432cac793c88a99240b320e69c0d7335a24
Natanael Junkes <natanael.junkes@gmail.com>
2020-03-13 15:45:41 -0300

    Terceiro Database de migração criado

--- DAO/Migrations/20200313184421_ThirdDatabase.Designer.cs
+++ DAO/Migrations/20200313184421_ThirdDatabase.Designer.cs
@@ -4,14 +4,16 @@ using DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAO.Migrations
 {
     [DbContext(typeof(QuadribolContext))]
-    partial class QuadribolContextModelSnapshot : ModelSnapshot
+    [Migration("20200313184421_ThirdDatabase")]
+    partial class ThirdDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAO/Migrations/20200313184421_ThirdDatabase.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAO.Migrations
+{
+    public partial class ThirdDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_COMPETIDORES_TIMES_TimeID",
+                table: "COMPETIDORES");
+
+            migrationBuilder.DropIndex(
+                name: "IX_COMPETIDORES_TimeID",
+                table: "COMPETIDORES");
+
+            migrationBuilder.DropColumn(
+                name: "TimeID",
+                table: "COMPETIDORES");
+
+            migrationBuilder.CreateTable(
+                name: "TimeCompetidor",
+                columns: table => new
+                {
+                    TimeID = table.Column<int>(nullable: false),
+                    CompetidorID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeCompetidor", x => new { x.TimeID, x.CompetidorID });
+                    table.ForeignKey(
+                        name: "FK_TimeCompetidor_COMPETIDORES_CompetidorID",
+                        column: x => x.CompetidorID,
+                        principalTable: "COMPETIDORES",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TimeCompetidor_TIMES_TimeID",
+                        column: x => x.TimeID,
+                        principalTable: "TIMES",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeCompetidor_CompetidorID",
+                table: "TimeCompetidor",
+                column: "CompetidorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeCompetidor");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeID",
+                table: "COMPETIDORES",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_COMPETIDORES_TimeID",
+                table: "COMPETIDORES",
+                column: "TimeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_COMPETIDORES_TIMES_TimeID",
+                table: "COMPETIDORES",
+                column: "TimeID",
+                principalTable: "TIMES",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
