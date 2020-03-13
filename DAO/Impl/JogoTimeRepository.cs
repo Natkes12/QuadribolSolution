eed949a3b524e88c7b70fccba0ee4cdcb12fb5c9
Natanael Junkes <natanael.junkes@gmail.com>
2020-03-13 15:53:26 -0300

    Repositório de JogoTime

---
+++ DAO/Impl/JogoTimeRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAO.Interfaces;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Impl
+{
+    public class JogoTimeRepository : IJogoTimeRepository
+    {
+        public Task<DataResponse<JogoTime>> GetData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(JogoTime jogoTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAO/Interfaces/IJogoTimeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Interfaces
+{
+    public interface IJogoTimeRepository
+    {
+        Task<Response> Insert(JogoTime jogoTime);
+        Task<DataResponse<JogoTime>> GetData();
+    }
+}
